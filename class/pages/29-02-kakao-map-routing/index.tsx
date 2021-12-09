6ee14b0dd43358386143f87298f032eab169427a
nwd0907 <nwd0907@newbizstart.com>
2021-12-09 15:53:21 +0900

    Update kakao-map

--- class/pages/29-02-kakao-map-routing/index.tsx
+++ class/pages/29-02-kakao-map-routing/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function KakaoMapRoutingPage() {
   const router = useRouter();
@@ -7,5 +8,17 @@ export default function KakaoMapRoutingPage() {
     router.push("/29-03-kakao-map-routed");
   }
 
-  return <button onClick={onClickMoveToMap}>맵으로 이동하기!!!</button>;
+  return (
+    <>
+      <button onClick={onClickMoveToMap}>
+        맵으로 이동하기!!!(router.push)
+      </button>
+      {/* <br />
+      <a href="/29-03-kakao-map-routed">맵으로 이동하기!!(a태그)</a>
+      <br />
+      <Link href="/29-03-kakao-map-routed">
+        <a>맵으로 이동하기!!(Link태그)</a>
+      </Link> */}
+    </>
+  );
 }
